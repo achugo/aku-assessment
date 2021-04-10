6dee2193a6d83fb506263af03b507c99b3b47001
achugo <emeka@brandmobileafrica.com>
2021-04-10 11:08:51 +0100

    nav scroll effect to navbar

--- src/templates/navbar/Navbar.js
+++ src/templates/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { ReactComponent as Logo } from "../../assets/svgs/logo.svg";
 import { ReactComponent as Button } from "../../assets/svgs/hamburger.svg";
@@ -6,7 +6,7 @@ import { ReactComponent as Button } from "../../assets/svgs/hamburger.svg";
 const Nav = styled.nav`
   margin-left: 0;
   width: 100%;
-  height: 200px;
+  height: 40px;
   position: fixed;
   z-index: 1;
 
@@ -20,8 +20,29 @@ const NavItems = styled.div`
 `;
 
 const Navbar = () => {
+  const [scrollState, setScrollState] = useState("");
+
+  useEffect(() => {
+    let listener = null;
+    listener = document.addEventListener("scroll", (e) => {
+      var scrolled = document.scrollingElement.scrollTop;
+      if (scrolled >= 100) {
+        if (scrollState !== "colored") {
+          setScrollState("colored");
+          console.log("scrolled!!");
+        }
+      } else {
+        if (scrollState !== "colored") {
+          setScrollState("transparent");
+        }
+      }
+    });
+    return () => {
+      document.removeEventListener("scroll", listener);
+    };
+  }, [scrollState]);
   return (
-    <Nav>
+    <Nav className={scrollState}>
       <div className="container">
         <NavItems>
           <Logo />
